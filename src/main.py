33bfeaef6e72916a2aebc8805d52f77eeb9d6b74
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-24 03:42:45 +0300

    Add /random

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from bot import bot
 from telebot.types import Message
+from words import random_pair
 
 @bot.message_handler(commands=['start'])
 def on_start(message: Message) -> None:
     bot.send_message(chat_id=message.from_user.id, text='Hello!')
+
+@bot.message_handler(commands=['random'])
+def on_random(message: Message) -> None:
+    bot.send_message(chat_id=message.from_user.id,
+                     text=random_pair())
 
 if __name__ == '__main__':
     bot.infinity_polling()
