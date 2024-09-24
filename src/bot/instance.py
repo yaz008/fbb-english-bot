f39fcb50c3712d78a314f9132cc7fb720937ccf7
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-24 03:07:16 +0300

    Change default parse_mode to MarkdownV2

--- src/bot/instance.py
+++ src/bot/instance.py
@@ -5,7 +5,7 @@ from telebot import TeleBot
 if not load_dotenv():
     raise Exception('Unable to load .env file')
 bot: TeleBot = TeleBot(token=getenv(key='TELEGRAM_API_KEY'),
-                       parse_mode='Markdown',
+                       parse_mode='MarkdownV2',
                        skip_pending=True,
                        threaded=False,
                        num_threads=1)
