044d172b2defd2376544cd403d49294b7fb34f78
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-24 03:44:41 +0300

    Update /start text

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ from words import random_pair
 
 @bot.message_handler(commands=['start'])
 def on_start(message: Message) -> None:
-    bot.send_message(chat_id=message.from_user.id, text='Hello!')
+    start_text: str = 'Welcome to *FBB English Bot*\\!\n\n'
+    start_text += 'Send /random to start practicing'
+    bot.send_message(chat_id=message.from_user.id,
+                     text=start_text)
 
 @bot.message_handler(commands=['random'])
 def on_random(message: Message) -> None:
