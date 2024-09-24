7a0fe5a9f545deadf56da8735a65a2bb07e8d2c8
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-24 04:04:12 +0300

    Add callback_query_handler

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from bot import bot
-from telebot.types import Message
+from telebot.types import Message, CallbackQuery
 from words import random_pair
 from markup import on_random_markup
 
@@ -15,6 +15,15 @@ def on_random(message: Message) -> None:
     bot.send_message(chat_id=message.from_user.id,
                      text=random_pair(),
                      reply_markup=on_random_markup())
+    
+@bot.callback_query_handler(func=lambda *_: True)
+def on_callback(callback: CallbackQuery) -> None:
+    if callback.data == 'next':
+        message: Message = callback.message
+        bot.edit_message_text(text=random_pair(),
+                              chat_id=message.chat.id,
+                              message_id=message.id,
+                              reply_markup=on_random_markup())
 
 if __name__ == '__main__':
     bot.infinity_polling()
